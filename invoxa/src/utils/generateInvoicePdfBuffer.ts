ee63e2546d5fd680921da3c067405ce5cf8f9d2c
Sasanka <sasankasekharkundu24@gmail.com>
2025-07-16 09:27:48 +0530

    Fix Some Errors and Released stable  version 0.2

--- invoxa/src/utils/generateInvoicePdfBuffer.ts
+++ invoxa/src/utils/generateInvoicePdfBuffer.ts
@@ -1,10 +1,16 @@
-import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 
 export async function generateInvoicePdfBuffer(html: string) {
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
   const page = await browser.newPage();
   await page.setContent(html, { waitUntil: 'networkidle0' });
-  const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+  const pdfBuffer = await page.pdf({ format: 'a4', printBackground: true });
   await browser.close();
   return pdfBuffer;
 }
